9660bed97ccdd5b9193ff381757a6a49062d76a2
chavely15k <chavely15k@gmail.com>
2024-02-16 15:06:29 -0500

    tests

--- tests/src/test_event_system.py
+++ tests/src/test_event_system.py
@@ -1,6 +1,17 @@
+import unittest
 
-def test():
-    sim = simulation(2, get_uniform, get_uniform)
-    sim.simulate(10)
 
-test()
+import sys
+import os
+
+sys.path.insert(0, os.getcwd() + '/src')
+
+from event_system import *
+
+class SimulationTest(unittest.TestCase):
+    def test_simulate(self):
+        sim = simulation(2, get_uniform, get_uniform)
+        sim.simulate(10)
+
+if __name__ == '__main__':
+    unittest.main()
